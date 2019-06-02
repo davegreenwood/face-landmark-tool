0bf255d13ccb07219e36240659074c02e0e650e9
Dave Greenwood <david.greenwood@uea.ac.uk>
2019-06-02 17:10:01 +0100

    working line group

--- flt.py
+++ flt.py
@@ -12,104 +12,187 @@ def read_json(fname):
     return data
 
 
-class Group(QtWidgets.QGraphicsItem):
-    """https://stackoverflow.com/a/32198716"""
+class Marker(QtWidgets.QGraphicsPathItem):
 
-    def __init__(self, parent=None):
-        super(Group, self).__init__(parent)
-        self.line_col = QtGui.QColor(0, 255, 0)
-        self.pen = QtGui.QPen(self.line_col, 0.5)
-        self.rect = QtCore.QRectF()
-        m1 = Marker(parent=self)
-        m2 = Marker(parent=self)
-        m2.setPos(20, 30)
-        self.markers = [m1, m2]
-        self.polyline = QtGui.QPolygonF()
-        self.setpolyline()
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable)
-        self.setZValue(10)
+    cross = QtGui.QPainterPath()
+    cross.addRect(QtCore.QRectF(0, -5, 0, 10))
+    cross.addRect(QtCore.QRectF(-5, 0, 10, 0))
 
-    def boundingRect(self):
-        return self.rect
+    square = QtGui.QPainterPath()
+    square.addRect(QtCore.QRectF(-5, -5, 10, 10))
+    square.addRect(QtCore.QRectF(0, -5, 0, 10))
+    square.addRect(QtCore.QRectF(-5, 0, 10, 0))
 
-    def paint(self, painter=None, style=None, widget=None):
-        painter.setPen(self.pen)
-        painter.drawPolyline(self.polyline)
+    red_pen = QtGui.QPen(QtGui.QColor("red"), 0.5)
+    grn_pen = QtGui.QPen(QtGui.QColor("green"), 0.5)
 
-    def setpolyline(self):
-        self.polyline.clear()
-        x, y = [], []
-        for m in self.markers:
-            p = m.pos()
-            x.append(p.x())
-            y.append(p.y())
-            self.polyline.append(p)
-        self.rect = QtCore.QRectF(
-            min(x) - 6, min(y) - 6,
-            max(x) - min(x) + 6,
-            max(y) - min(y) + 6)
+    def __init__(self, annotation_item, index):
+        super(Marker, self).__init__()
+        self.m_annotation_item = annotation_item
+        self.m_index = index
 
+        self.setPath(Marker.cross)
+        self.setBrush(QtGui.QBrush(QtCore.Qt.NoBrush))
+        self.setPen(self.grn_pen)
 
-class Marker(QtWidgets.QGraphicsObject):
-    """
-    Draggable - https://stackoverflow.com/a/28519324
-    """
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable, True)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemSendsGeometryChanges, True)
+        self.setAcceptHoverEvents(True)
+        self.setZValue(11)
+        self.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
-    def __init__(self, parent=None):
-        super(Marker, self).__init__(parent)
-        self.parent = parent
-        self.fill_col = QtGui.QColor(0, 255, 0, 50)
-        self.line_col = QtGui.QColor(0, 255, 0)
-        self.pen = QtGui.QPen(self.line_col, 0.5)
-        self.rect = QtCore.QRectF(-6, -6, 12, 12)
-        self.line1 = QtCore.QLineF(0, 0, 0, 5)
-        self.line2 = QtCore.QLineF(0, 0, 0, -5)
-        self.line3 = QtCore.QLineF(0, 0, 5, 0)
-        self.line4 = QtCore.QLineF(0, 0, -5, 0)
-        self.setCursor(QtCore.Qt.OpenHandCursor)
-        self.setAcceptedMouseButtons(QtCore.Qt.LeftButton)
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable)
-        self.setZValue(10)
+    def hoverEnterEvent(self, event):
+        self.setPath(Marker.square)
+        self.setPen(self.red_pen)
+        self.setBrush(QtGui.QColor(255, 0, 0, 32))
+        super(Marker, self).hoverEnterEvent(event)
 
-    def boundingRect(self):
-        return self.rect
-
-    def paint(self, painter=None, style=None, widget=None):
-        painter.setPen(self.pen)
-        for line in [self.line1, self.line2, self.line3, self.line4]:
-            painter.drawLine(line)
-        painter.fillRect(self.rect, self.fill_col)
-
-    def mouseMoveEvent(self, event):
-        if (QtCore.QLineF(
-            QtCore.QPointF(event.screenPos()),
-            QtCore.QPointF(event.buttonDownScreenPos(
-                QtCore.Qt.LeftButton))).length() <
-                QtWidgets.QApplication.startDragDistance()):
-            return
-        if self.parent:
-            self.parent.setpolyline()
-        drag = QtGui.QDrag(event.widget())
-        drag.exec_()
-        self.setCursor(QtCore.Qt.OpenHandCursor)
-        super(Marker, self).mouseMoveEvent(event)
-
-    def mousePressEvent(self, event):
-        self.setCursor(QtCore.Qt.ClosedHandCursor)
-        super(Marker, self).mousePressEvent(event)
+    def hoverLeaveEvent(self, event):
+        self.setPath(Marker.cross)
+        self.setPen(self.grn_pen)
+        self.setBrush(QtGui.QBrush(QtCore.Qt.NoBrush))
+        super(Marker, self).hoverLeaveEvent(event)
 
     def mouseReleaseEvent(self, event):
-        self.setCursor(QtCore.Qt.OpenHandCursor)
+        self.setSelected(False)
         super(Marker, self).mouseReleaseEvent(event)
 
+    def itemChange(self, change, value):
+        if (change == QtWidgets.QGraphicsItem.ItemPositionChange and
+                self.isEnabled()):
+            self.m_annotation_item.movePoint(self.m_index, value)
+        return super(Marker, self).itemChange(change, value)
 
-class imageLabeler(QtWidgets.QMainWindow):
+
+class LineGroup(QtWidgets.QGraphicsPathItem):
+
+    red_pen = QtGui.QPen(QtGui.QColor("red"), 0.5)
+    grn_pen = QtGui.QPen(QtGui.QColor("green"), 0.5)
+
+    def __init__(self, parent=None):
+        super(LineGroup, self).__init__(parent)
+        self.setPen(self.grn_pen)
+        self.setBrush(QtGui.QBrush(QtCore.Qt.NoBrush))
+        self.setZValue(10)
+        self.setAcceptHoverEvents(True)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable, True)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemSendsGeometryChanges, True)
+        self.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.m_points = []
+        self.m_items = []
+
+    def set_path(self):
+        painter_path = QtGui.QPainterPath()
+        painter_path.addPolygon(QtGui.QPolygonF(self.m_points))
+        self.setPath(painter_path)
+
+    def addPoint(self, p):
+        self.m_points.append(p)
+        self.set_path()
+        item = Marker(self, len(self.m_points) - 1)
+        self.scene().addItem(item)
+        self.m_items.append(item)
+        item.setPos(p)
+
+    def movePoint(self, index, pos):
+        if 0 <= index < len(self.m_points):
+            self.m_points[index] = self.mapFromScene(pos)
+            self.set_path()
+
+    def move_item(self, index, pos):
+        if 0 <= index < len(self.m_items):
+            item = self.m_items[index]
+            item.setEnabled(False)
+            item.setPos(pos)
+            item.setEnabled(True)
+
+    def itemChange(self, change, value):
+        if change == QtWidgets.QGraphicsItem.ItemPositionHasChanged:
+            for i, point in enumerate(self.m_points):
+                self.move_item(i, self.mapToScene(point))
+        return super(LineGroup, self).itemChange(change, value)
+
+    def hoverEnterEvent(self, event):
+        self.setPen(self.red_pen)
+        super(LineGroup, self).hoverEnterEvent(event)
+
+    def hoverLeaveEvent(self, event):
+        self.setPen(self.grn_pen)
+        super(LineGroup, self).hoverLeaveEvent(event)
+
+
+# -----------------------------------------------------------------------------
+# Graphics Scene
+# -----------------------------------------------------------------------------
+
+
+
+# -----------------------------------------------------------------------------
+# Graphics View
+# -----------------------------------------------------------------------------
+
+
+class ImageView(QtWidgets.QGraphicsView):
+
+    def __init__(self, parent):
+        super(ImageView, self).__init__(parent)
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.image = QtWidgets.QGraphicsPixmapItem()
+        self.scene.addItem(self.image)
+        self.line = LineGroup()
+        self.scene.addItem(self.line)
+        for x, y in [[10, 20], [20, 30], [30, 40]]:
+            self.line.addPoint(QtCore.QPointF(x, y))
+        self.setScene(self.scene)
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(30, 30, 30)))
+        self.setFrameShape(QtWidgets.QFrame.NoFrame)
+
+    def fitInView(self, scale=True):
+        rect = QtCore.QRectF(self.image.pixmap().rect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+        self.resetTransform()
+        self.scale(1.0, 1.0)
+
+    def setPhoto(self, pixmap=None):
+        if pixmap and not pixmap.isNull():
+            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+            self.image.setPixmap(pixmap)
+        else:
+            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+            self.image.setPixmap(QtGui.QPixmap())
+        self.fitInView()
+
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            factor = 1.25
+        else:
+            factor = 0.8
+        self.scale(factor, factor)
+
+    def toggleDragMode(self):
+        if self.dragMode() == QtWidgets.QGraphicsView.ScrollHandDrag:
+            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+        elif not self.image.pixmap().isNull():
+            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+
+
+# -----------------------------------------------------------------------------
+# Window Class
+# -----------------------------------------------------------------------------
+
+
+class imageLabelerWindow(QtWidgets.QMainWindow):
     """The main application window with menu items."""
 
     def __init__(self):
-        super(imageLabeler, self).__init__()
+        super(imageLabelerWindow, self).__init__()
         self.viewer = ImageView(self)
         self.setCentralWidget(self.viewer)
         self.createMenus()
@@ -168,66 +251,10 @@ class imageLabeler(QtWidgets.QMainWindow):
             return
         self.viewer.setPhoto(QtGui.QPixmap.fromImage(image))
 
-# -----------------------------------------------------------------------------
-# Graphics View
-# -----------------------------------------------------------------------------
-
-
-class ImageView(QtWidgets.QGraphicsView):
-
-    def __init__(self, parent):
-        super(ImageView, self).__init__(parent)
-        self.scene = QtWidgets.QGraphicsScene(self)
-        self.image = QtWidgets.QGraphicsPixmapItem()
-        self.scene.addItem(self.image)
-        self.scene.addItem(Group())
-        self.setScene(self.scene)
-        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
-        self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
-        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(30, 30, 30)))
-        self.setFrameShape(QtWidgets.QFrame.NoFrame)
-
-    def fitInView(self, scale=True):
-        rect = QtCore.QRectF(self.image.pixmap().rect())
-        if not rect.isNull():
-            self.setSceneRect(rect)
-        self.resetTransform()
-        self.scale(1.0, 1.0)
-
-    def setPhoto(self, pixmap=None):
-        if pixmap and not pixmap.isNull():
-            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
-            self.image.setPixmap(pixmap)
-        else:
-            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
-            self.image.setPixmap(QtGui.QPixmap())
-        self.fitInView()
-
-    def wheelEvent(self, event):
-        if event.angleDelta().y() > 0:
-            factor = 1.25
-        else:
-            factor = 0.8
-        self.scale(factor, factor)
-
-    # def mousePressEvent(self, event):
-    #     self.setCursor(QtCore.Qt.ClosedHandCursor)
-
-    # def mouseReleaseEvent(self, event):
-    #     self.setCursor(QtCore.Qt.OpenHandCursor)
-
-    def toggleDragMode(self):
-        if self.dragMode() == QtWidgets.QGraphicsView.ScrollHandDrag:
-            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
-        elif not self.image.pixmap().isNull():
-            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
-
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(["Face Label Tool"])
     path = resource_filename(__name__, "icon.png")
     app.setWindowIcon(QtGui.QIcon(path))
-    ui = imageLabeler()
+    ui = imageLabelerWindow()
     sys.exit(app.exec_())
